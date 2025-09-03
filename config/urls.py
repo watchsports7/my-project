1da8b9cebf4d1f5b9768ea3f315201a6f798e01d
watchsports7 <129778556+watchsports7@users.noreply.github.com>
2025-09-03 19:36:44 +0300

    Delete config directory

--- config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from news import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.article_list, name='home'),  # Главная → список статей
-    path('news/', include('news.urls')),         # Доп. страницы приложения
-]
