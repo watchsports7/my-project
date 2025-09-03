c5c3c4f36081da09cdf32c60c8d78b5f58cfc948
watchsports7 <129778556+watchsports7@users.noreply.github.com>
2025-09-03 19:57:00 +0300

    Delete news directory

--- news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.article_list, name='article_list'),
-    path('article/<int:pk>/', views.article_detail, name='article_detail'),
-    path('category/<int:category_id>/', views.category_articles, name='category_articles'),
-]
