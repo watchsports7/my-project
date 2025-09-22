e1978859b45d96300a5127de12c12ee8483edbce
watchsports7 <129778556+watchsports7@users.noreply.github.com>
2025-09-22 07:54:24 +0300

    Delete news directory

--- news/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Article
-
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'is_featured', 'published_at', 'created_at')
-    list_filter = ('is_featured',)
-    search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title',)}
