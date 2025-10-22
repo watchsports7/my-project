a1c3246f3afe3bb4822317341f9067426eac2f43
watchsports7 <129778556+watchsports7@users.noreply.github.com>
2025-10-22 20:06:10 +0300

    Delete core directory

--- core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Кастомизация админки
-admin.site.site_header = "InsideSport — Админка"
-admin.site.site_title = "InsideSport"
-admin.site.index_title = "Добро пожаловать в панель управления"
